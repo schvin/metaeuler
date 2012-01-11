087d4b20400a84f3da2cb7f9c12d0edd265bef72
George Lewis <schvin@schvin.net>
2012-01-11 17:05:28 +0000

    initial notes

---
+++ bin/draw-straws.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+# extract language list from https://projecteuler.net/languages; support blacklist
+# static list of friends
+# list their problems; find set of not-solved for all friends, and generate list
