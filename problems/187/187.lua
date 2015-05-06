b6ebac529514aff591ac584afbe12b948637fa91
George Lewis <schvin@schvin.net>
2015-05-06 12:42:48 -0400

    stub

--- problems/187/187.lua
+++ problems/187/187.lua
@@ -1,3 +1,12 @@
 -- problem 187
 
+
+-- function upper(x, y)
+--  return(x ^ y)
+--end
+
 io.write("running test 187...\n")
+max = 30
+
+--a = upper(10, 8)
+--io.write(string.format("test run for to a max of %d\n", a))
